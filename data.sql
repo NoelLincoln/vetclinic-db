dfe1a2422a9be1ce7c75920c80ce68f76caf1ea0
Noel Bryant <noelsobryant@gmail.com>
2023-11-07 21:41:56 +0300

    add more animals data

--- data.sql
+++ data.sql
@@ -7,3 +7,16 @@ INSERT INTO animals VALUES
 -- add species column
 ALTER TABLE animals
 ADD COLUMN species varchar(255);
+
+-- Insert new animals data
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES
+    (5,'Charmander',DATE '2020-02-08' ,0, false, -11),
+    (6,'Plantmon',DATE '2021-11-15',  2,true, -5.7),
+    (7,'Squirtle',DATE '1993-04-02', 3 ,false,-12.13),
+    (8,',Angemon', DATE '2005-06-12',1, true,  -45),
+    (9,',Boarmon',DATE '2005-06-07',7, true,  20.4),
+    (10,'Blossom',DATE '1998-10-13',3, true,  17),
+    (11,'Ditto', DATE '2022-05-14', 4, true, 22);
+
+
